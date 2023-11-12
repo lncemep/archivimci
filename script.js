abc2495ac75b7c24eda9b8c66345bd9a129a6f24
Dima <75248183+lncemep@users.noreply.github.com>
2023-11-12 22:24:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ reloadButton.addEventListener('click', function () {
     // Use the location object to reload the current page
     location.reload();
 });
+
+// Add an event listener to the "Dalej" button
+var reloadButton = document.getElementById('flipCard');
+reloadButton.addEventListener('click', function () {
+    // Hide the category container
+    var categoryContainer = document.getElementById('category-container');
+    categoryContainer.style.display = 'none';
+
+    // Remove the event listener after hiding the categories
+    reloadButton.removeEventListener('click', arguments.callee);
+});
